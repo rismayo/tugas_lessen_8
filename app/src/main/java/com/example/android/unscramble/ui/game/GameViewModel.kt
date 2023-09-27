b8226d71030d078267f0947884ad00eab5b3ea33
rismayo <rismamanura855@gmail.com>
2023-09-27 14:08:26 +0700

    menambahkan blok init sebagai tempat penyiapan awal

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+    }
 
     private var score = 0
     private var currentWordCount = 0

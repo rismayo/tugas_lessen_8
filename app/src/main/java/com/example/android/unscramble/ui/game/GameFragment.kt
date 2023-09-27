de8e2020d0f26a9378e31046755f3a97697da58a
RahmadinaMFA <rahmadinaandani24@gmail.com>
2023-09-27 18:54:53 +0700

    Jadikan dialog pemberitahuan tidak dapat dibatalkan saat tombol kembali ditekan, menggunakan metode setCancelable() dan meneruskan false.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -96,6 +96,7 @@ class GameFragment : Fragment() {
         MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.congratulations))
             .setMessage(getString(R.string.you_scored, viewModel.score))
+            .setCancelable(false)
     }
     /*
      * Re-initializes the data in the ViewModel and updates the views with the new data, to

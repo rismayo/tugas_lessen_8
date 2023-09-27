bae5a342c985afe2ebadcfb7b4b2e6fe9fab6f4b
rismayo <rismamanura855@gmail.com>
2023-09-27 14:15:10 +0700

    menambahkan laporan log di dalam onCleared() untuk melacak siklus proses GameViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -13,4 +13,9 @@ class GameViewModel : ViewModel() {
     private var _currentScrambledWord = "test"
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
 }

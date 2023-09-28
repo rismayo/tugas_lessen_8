9ddfe7cfaf42f82d949cb7082525bf83706fd25b
rismayo <rismamanura855@gmail.com>
2023-09-28 20:34:33 +0700

    melampirkan observer untuk currentWordCount LiveData. Teruskan viewLifecycleOwner sebagai parameter pertama ke observer dan ekspresi lambda untuk parameter kedua. Di dalam ekspresi lambda, teruskan jumlah kata baru sebagai parameter dan di isi fungsi, tetapkan jumlah kata baru dengan MAX_NO_OF_WORDS pada tampilan teks.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -67,6 +67,11 @@ class GameFragment : Fragment() {
             { newScore ->
                 binding.score.text = getString(R.string.score, newScore)
             })
+        viewModel.currentWordCount.observe(viewLifecycleOwner,
+            { newWordCount ->
+                binding.wordCount.text =
+                    getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
+            })
     }
 
     override fun onDetach() {
